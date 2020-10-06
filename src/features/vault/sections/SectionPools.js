f24bcde83323f8cfe21c2013f87be7e444cb806b
bucketYang <515971947@qq.com>
2020-10-06 15:43:19 +0800

    update sth

--- src/features/vault/sections/SectionPools.js
+++ src/features/vault/sections/SectionPools.js
@@ -312,7 +312,7 @@ export default function SectionPools() {
                             </Hidden>
                           </Typography>
                           <Typography className={classes.iconContainerSubTitle} variant="body2">{pool.tokenDescription}</Typography>
-                          {pool.token === "RI" ? (<Typography className={classes.iconContainerSubTitle} variant="body2">(require 100DARK)</Typography>):''}
+                          {pool.token === "RI" ? (<Typography className={classes.iconContainerSubTitle} variant="body2">(require 100 DARK)</Typography>):''}
                         </Grid>
                       </Grid>
                     </Grid>
